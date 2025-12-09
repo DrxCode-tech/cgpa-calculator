c0653e7322a3f72d86c649d99507659c0c60bef6
DrxCode-tech <anietiedaniel11@gmail.com>
2025-12-09 02:53:34 +0100

    all

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,11 @@ export default defineConfig({
         globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
       },
 
-      includeAssets: ['favicon.ico'],
+      includeAssets: [
+        'favicon.ico',
+        'icons/icon-192.png',
+        'icons/icon-512.png'
+      ],
 
       manifest: {
         name: 'CGPA Calculator',
@@ -27,12 +31,14 @@ export default defineConfig({
           {
             src: 'icons/icon-192.png',
             sizes: '192x192',
-            type: 'image/png'
+            type: 'image/png',
+            purpose: 'any maskable'
           },
           {
             src: 'icons/icon-512.png',
             sizes: '512x512',
-            type: 'image/png'
+            type: 'image/png',
+            purpose: 'any maskable'
           }
         ]
       }
