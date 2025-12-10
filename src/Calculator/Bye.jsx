f9624d4e2db6d4da86142b15e8d867e279b10f31
DrxCode-tech <anietiedaniel11@gmail.com>
2025-12-10 03:47:13 +0100

    njg

--- src/Calculator/Bye.jsx
+++ src/Calculator/Bye.jsx
@@ -3,21 +3,28 @@ import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
 import "./All.css";
 
-export default function Bye(){
+export default function Bye() {
     const navigate = useNavigate();
-    useEffect(()=>{
-        setTimeout(()=> window.location.replace("about:blank"),3000)
-    },[])
+    useEffect(() => {
+        setTimeout(() => {
+            if (window.matchMedia('(display-mode: standalone)').matches) {
+                window.close();
+            } else {
+                window.location.replace("about:blank");
+            }
+        }, 3000);
+    }, []);
+
     return (
-        <motion.div 
-        initial={{y:150 , opacity:0}}
-        animate={{y:0, opacity:1}}
-        transition={{
-            duration:5,
-            stiffness:300,
-            type:"spring"
-        }}
-        className="greet">
+        <motion.div
+            initial={{ y: 150, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{
+                duration: 5,
+                stiffness: 300,
+                type: "spring"
+            }}
+            className="greet">
             <div className="greet-text">
                 Okay Thanks for trying out the CGPA Calculator App.
             </div>
